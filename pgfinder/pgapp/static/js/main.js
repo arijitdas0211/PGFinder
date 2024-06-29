6c198946f8002d492b9e2797606aaa4633234c70
Arijit Das <143489977+arijitdas0211@users.noreply.github.com>
2024-06-29 23:32:15 +0530

    FinalUpdate

--- pgfinder/pgapp/static/js/main.js
+++ pgfinder/pgapp/static/js/main.js
@@ -74,59 +74,23 @@ datatables.forEach(datatable => {
 
 // User star rating
 $(document).ready(function () {
-  a = $('#one')
-  b = $('#two')
-  c = $('#three')
-  d = $('#four')
-  e = $('#five')
-  x = 0
+  var elements = ['#one', '#two', '#three', '#four', '#five'];
+  var x = 0;
 
-  $(a).click(function () {
-    $(a).css("color", "#FFBB00")
-    $(b).css("color", "initial")
-    $(c).css("color", "initial")
-    $(d).css("color", "initial")
-    $(e).css("color", "initial")
-    x = 1
-  })
-  $(b).click(function () {
-    $(a).css("color", "#FFBB00")
-    $(b).css("color", "#FFBB00")
-    $(c).css("color", "initial")
-    $(d).css("color", "initial")
-    $(e).css("color", "initial")
-    x = 2
-  })
-  $(c).click(function () {
-    $(a).css("color", "#FFBB00")
-    $(b).css("color", "#FFBB00")
-    $(c).css("color", "#FFBB00")
-    $(d).css("color", "initial")
-    $(e).css("color", "initial")
-    x = 3
-  })
-  $(d).click(function () {
-    $(a).css("color", "#FFBB00")
-    $(b).css("color", "#FFBB00")
-    $(c).css("color", "#FFBB00")
-    $(d).css("color", "#FFBB00")
-    $(e).css("color", "initial")
-    x = 4
-  })
-  $(e).click(function () {
-    $(a).css("color", "#FFBB00")
-    $(b).css("color", "#FFBB00")
-    $(c).css("color", "#FFBB00")
-    $(d).css("color", "#FFBB00")
-    $(e).css("color", "#FFBB00")
-    x = 5
-  })
+  $(elements.join(',')).click(function () {
+    var index = elements.indexOf('#' + $(this).attr('id'));
+    x = index + 1;
+
+    for (var i = 0; i < elements.length; i++) {
+      $(elements[i]).css("color", i <= index ? "#FFBB00" : "initial");
+    }
+  });
+
   $('.val').click(function () {
-    // console.log(x)
-    var score = x
-    $('#val').attr("value", score);
-  })
+    $('#val').val(x); // Assuming you want to set the value of an input with id="val"
+  });
 });
+
 
 // OWL Carousel
 $(document).ready(function () {
